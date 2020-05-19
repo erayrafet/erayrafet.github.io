f1241e4233ececcbfab118a271f8cba7e00a8f7a
Eray Rafet <erayrafet@gmail.com>
2020-05-19 12:07:19 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,33 +1,37 @@
-self.addEventListener('install', (event) => {
-  console.log('Inside the install handler:', event);
+self.addEventListener('install', function(event) {
+    self.skipWaiting();
+    var offlinePage = new Request('offline.html');
+    event.waitUntil(fetch(offlinePage).then(function(response) {
+        return caches.open('offline2').then(function(cache) {
+            return cache.put(offlinePage, response);
+        });
+    }));
 });
-
-self.addEventListener('activate', (event) => {
-  console.log('Inside the activate handler:', event);
+self.addEventListener('fetch', function(event) {
+    event.respondWith(fetch(event.request).catch(function(error) {
+        return caches.open('offline2').then(function(cache) {
+            return cache.match('offline.html');
+        });
+    }));
 });
-
-self.addEventListener(fetch, (event) => {
-  console.log('Inside the fetch handler:', event);
+self.addEventListener('refreshOffline', function(response) {
+    return caches.open('offline2').then(function(cache) {
+        return cache.put(offlinePage, response);
+    });
 });
-
-var get = function(url) {
-  return new Promise(function(resolve, reject) {
-
-    var xhr = new XMLHttpRequest();
-    xhr.onreadystatechange = function() {
-        if (xhr.readyState === XMLHttpRequest.DONE) {
-            if (xhr.status === 200) {
-                var result = xhr.responseText
-                result = JSON.parse(result);
-                resolve(result);
-            } else {
-                reject(xhr);
-            }
+self.addEventListener('push', function(event) {
+    var data = event.data.json();
+    var opts = {
+        body: data.body,
+        icon: data.icon,
+        data: {
+            url: data.url
         }
     };
-    
-    xhr.open("GET", url, true);
-    xhr.send();
-
-  }); 
-};
+    event.waitUntil(self.registration.showNotification(data.title, opts));
+});
+self.addEventListener('notificationclick', function(event) {
+    var data = event.notification.data;
+    event.notification.close();
+    event.waitUntil(clients.openWindow(data.url));
+});
