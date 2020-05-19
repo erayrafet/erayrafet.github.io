d2ac96f24cdf15dafd43b32d29caf448f27b7306
Eray Rafet <erayrafet@gmail.com>
2020-05-19 11:51:11 +0300

    Add 200 response code

--- sw.js
+++ sw.js
@@ -9,3 +9,25 @@ self.addEventListener('activate', (event) => {
 self.addEventListener(fetch, (event) => {
   console.log('Inside the fetch handler:', event);
 });
+
+var get = function(url) {
+  return new Promise(function(resolve, reject) {
+
+    var xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState === XMLHttpRequest.DONE) {
+            if (xhr.status === 200) {
+                var result = xhr.responseText
+                result = JSON.parse(result);
+                resolve(result);
+            } else {
+                reject(xhr);
+            }
+        }
+    };
+    
+    xhr.open("GET", url, true);
+    xhr.send();
+
+  }); 
+};
