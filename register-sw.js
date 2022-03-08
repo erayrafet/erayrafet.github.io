6bbfa5337f6f08a7ce93674622050ca142e214ce
Eray Rafet <erayrafet@gmail.com>
2022-03-08 23:18:14 +0200

    Re-enable Service worker

--- register-sw.js
+++ register-sw.js
@@ -1,11 +1,3 @@
-/*
-if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('sw.js')
-  .then(function(registration) {
-    console.log('Registration successful, scope is:', registration.scope);
-  })
-  .catch(function(error) {
-    console.log('Service worker registration failed, error:', error);
-  });
+if ("serviceWorker" in navigator) {
+  navigator.serviceWorker.register("./sw.js");
 }
-*/
